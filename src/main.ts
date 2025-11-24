928dd9b7e9abb34d78a8125af909661494078bbe
JackyView <jackyview2004@163.com>
2025-11-24 23:12:56 +0800

    feat-重构了目录，配置了开发所需的依赖包，整理文件并删除了不需要的文件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
