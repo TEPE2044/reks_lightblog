5f965bf7132e729136c4a32cc7d5abb87c7ce4be
JackyView <jackyview2004@163.com>
2025-11-26 23:28:35 +0800

    feat:新建全局自定义样式global.scss 设计了ProfileCard和Aside的样式

--- src/Components/Aside.vue
+++ src/Components/Aside.vue
@@ -1,30 +1,14 @@
 <script setup lang="ts">
-// 博客包揽了图片上传和富文本编辑功能（内容不限），电台则支持音频上传
-const what = 'blog' // 'blog' | 'radio'
+import ProfileCard from '../UI/ProfileCard.vue';
+
+
 </script>
 
 <template>
   <div class="aside mt-2">
-    <BButton variant="success">一键签到</BButton>
-    <BButtonGroup>
-      <BButton variant="outline-primary">发布博客</BButton>
-      <BButton variant="outline-primary">发布电台</BButton>
-    </BButtonGroup>
-
-    <BCard>
-      <BListGroup flush>
-        待定内容板块
-        <BListGroupItem>
-          <BImg lazy thumbnail src="https://picsum.photos/1000/300"/>
-        </BListGroupItem>
-        <BListGroupItem>
-          <BImg lazy thumbnail src="https://picsum.photos/1000/301"/>
-        </BListGroupItem>
-        <BListGroupItem>
-          <BImg lazy thumbnail src="https://picsum.photos/1000/302"/>
-        </BListGroupItem>
-      </BListGroup>
-    </BCard>
+    <div class="profile-card">
+      <ProfileCard />
+    </div>
   </div>
 </template>
 
--- src/UI/ProfileCard.vue
+++ src/UI/ProfileCard.vue
@@ -1,22 +1,50 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const metricsData = ref([
+  { label: '订阅', value: 0 },
+  { label: '博客', value: 0 },
+  { label: '积分', value: 0 },
+]);
 </script>
 
 
 <template>
-  <div class="profileCard">
-    <BAvatar size="3rem" class="avatar" variant="light" />
+  <div class="profileCard d-flex flex-column align-items-center justify-content-center gap-3">
+    <div class="userInfo d-flex flex-row gap-4 justify-content-center align-items-center mt-3 pb-3">
+      <div class="avatar">
+        <BAvatar size="3rem" class="avatar" variant="secondary" />
+      </div>
+
+      
+      <BButton variant="success">一键签到</BButton>
+    </div>
+    <!-- 用户登陆后才出现该选项 -->
+    <div class="metrics-bar d-flex flex-row gap-5">
+      <div class="metrics-content d-flex flex-column align-items-center  mt-2 mb-4" :key="metrics.label" v-for="metrics in metricsData"> 
+        <div class="value">{{ metrics.value }}</div>
+        <div class="label">{{ metrics.label }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
+//用新语法@use代替旧语法@import
+@use "../Asset/CustomStyle/global.scss";
+
 .profileCard {
-  display: flex;
-  cursor: pointer;
-  .avatar{
-    border: 0.3px solid rgba(128, 128, 128, 0.21);
-    padding: 0.2rem;
-  }
+  @extend %reks-card-box;
+  border: 1px solid #e0e0e0;
 }
 
+.avatar {
+  @extend %reks-avatar;
+}
+
+.label{
+  //只有 white-space: nowrap; 能保证一行显示。
+  font-size: small;
+  white-space: nowrap;
+}
 </style>
