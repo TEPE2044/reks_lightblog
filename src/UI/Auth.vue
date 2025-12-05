59f86b4041c404b525a3f37f5919b5e4afc64add
JackyView <jackyview2004@163.com>
2025-12-05 10:54:03 +0800

    update:更新登录逻辑

--- src/UI/Auth.vue
+++ src/UI/Auth.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
-import { useToast, useToggle} from "bootstrap-vue-next";
-import { createToast } from '../Utils/reks-toast'
+import { useToast, useToggle } from "bootstrap-vue-next";
+import { createToast } from "../Utils/reks-toast";
 import { reactive, ref } from "vue";
 import Vcode from "vue3-puzzle-vcode";
-// modal打开逻辑 easy-modal 
+import { accountValidation,phoneValidation } from "../Utils/reks-login-regex";
+// modal打开逻辑 easy-modal
 // const {show, hide} = useToggle(id)
-const emd = useToggle('easy-login-box')
+const emd = useToggle("easy-login-box");
 const toast = useToast();
 
 // puzzle打开逻辑
 const isShow = ref(false);
 
 const reset = () => {
-  formData.account = '';
-  formData.password = '';
-  phoneData.phone = '';
-  phoneData.code = '';
-}
+  formData.account = "";
+  formData.password = "";
+  phoneData.phone = "";
+  phoneData.code = "";
+};
 
 const onShow = () => {
   isShow.value = true;
@@ -30,81 +31,167 @@ const onClose = () => {
 };
 
 const onSuccess = () => {
-  createToast(toast,'登录成功','欢迎回来','danger');
+  createToast(toast, "登录成功", "欢迎回来", "danger");
   onClose();
 };
 
-//
-const formData = reactive({
-  account: '',
-  password: ''
-})
+// 区分原生的FormData API和我们自己的数据结构
+interface FormData {
+  account: string;
+  password: string;
+}
 
-const phoneData = reactive({
-  phone: '',
-  code: ''
-})
+interface PhoneData {
+  phone: string;
+  code: string ;
+}
+const formData = reactive<FormData>({
+  account: "",
+  password: "",
+});
+
+const phoneData = reactive<PhoneData>({
+  phone: "",
+  code: "",
+});
+
+
+const loginbyAccount = () =>{
+
+  const accountError = accountValidation(formData.account, formData.password) ? "" : "请输入有效的账号和密码";
+
+  if (accountError) {
+    createToast(toast, "登录失败", accountError, "warning");
+    return;
+  }
+  onShow();
+  createToast(toast, "登录成功", "欢迎回来", "success");
+  reset();
+  emd.hide();
+}
+
+const loginbyPhone = () => {
+  const phoneError = phoneValidation(phoneData.phone) ? "" : "请输入有效的手机号";
+  if (phoneError) {
+    createToast(toast, "登录失败", phoneError, "warning");
+    return;
+  }
+  onShow();
+  createToast(toast, "登录成功", "欢迎回来", "success");
+  reset();
+  emd.hide();
+}
 </script>
 
 <template>
   <div class="auth">
     <BButton @click="emd.show()" variant="primary">登录</BButton>
 
-    <BModal id="easy-login-box" title="登录" ok-title="登录" no-header-close>
-      <div class="lgo d-flex align-items-center justify-content-center gap-2 mb-4">
-        <img src="/reks.svg" class="img-thumbnail top_pic mb-3" alt="reks_label.png" />
+    <BModal id="easy-login-box" title="登录" ok-title="登录" no-header-close centered>
+      <div
+        class="lgo d-flex align-items-center justify-content-center gap-2 mb-4"
+      >
+        <img
+          src="/reks.svg"
+          class="img-thumbnail top_pic mb-3"
+          alt="reks_label.png"
+        />
         <figure class="figure">
           <figcaption>Rekindle Everything</figcaption>
         </figure>
       </div>
-      <BTabs content-class="mt-3" align="center" lazy>
+      <BTabs content-class="mt-3" align="center" lazy pills>
         <BTab title="短信登录">
           <div class="form w-75 mx-auto">
             <BForm class="mx-auto">
-
-              <BFormFloatingLabel class="mt-2 mb-3" label="手机号" label-for="user-phone">
-                <BFormInput type="email" id="user-phone" placeholder="请输入手机号" v-model="phoneData.phone" required/>
+              <BFormFloatingLabel
+                class="mt-4 mb-3"
+                label="手机号"
+                label-for="user-phone"
+              >
+                <BFormInput
+                  type="email"
+                  id="user-phone"
+                  placeholder="请输入手机号"
+                  v-model="phoneData.phone"
+                  required
+                />
               </BFormFloatingLabel>
-
 
               <BInputGroup class="mb-2">
                 <BFormFloatingLabel label="验证码" label-for="user-code">
-                  <BFormInput type="text" id="user-code" placeholder="请输入验证码" v-model="phoneData.code" required/>
+                  <BFormInput
+                    type="text"
+                    id="user-code"
+                    placeholder="请输入验证码"
+                    v-model="phoneData.code"
+                    required
+                  />
                 </BFormFloatingLabel>
 
-                <BButton :disabled="phoneData.phone.length < 11" text="button" variant="primary">获取验证码</BButton>
+                <BButton
+                  :disabled="phoneData.phone.length < 11"
+                  text="button"
+                  variant="primary"
+                  >获取验证码</BButton
+                >
               </BInputGroup>
 
-
-              <BButton  class="w-100 mt-5" variant="primary" @click="onShow">登录</BButton>
+              <BButton class="w-100 mt-5" variant="primary" @click="loginbyPhone"
+                >登录</BButton
+              >
             </BForm>
           </div>
         </BTab>
 
-
-        <BTab title="密码登录">
+        <BTab title="账号登录">
           <div class="form w-75 mx-auto">
             <BForm class="mx-auto">
-
-              <BFormFloatingLabel class="mt-2 mb-3" label="账号" label-for="user-account">
-                <BFormInput type="email" id="user-account" placeholder="请输入账号" v-model="formData.account" />
+              <BFormFloatingLabel
+                class="mt-4 mb-3"
+                label="账号"
+                label-for="user-account"
+              >
+                <BFormInput
+                  type="email"
+                  id="user-account"
+                  placeholder="请输入账号"
+                  v-model="formData.account"
+                />
               </BFormFloatingLabel>
 
-              <BFormFloatingLabel class="mb-2" label="密码" label-for="user-password">
-                <BFormInput type="password" id="user-password" placeholder="请输入密码" v-model="formData.password" />
+              <BFormFloatingLabel
+                class="mb-2"
+                label="密码"
+                label-for="user-password"
+              >
+                <BFormInput
+                  type="password"
+                  id="user-password"
+                  placeholder="请输入密码"
+                  v-model="formData.password"
+                />
               </BFormFloatingLabel>
 
+              <BButton class="w-100 mt-5" variant="primary" @click="loginbyAccount"
+                >登录</BButton
+              >
 
-              <BButton  class="w-100 mt-5" variant="primary" @click="onShow">登录</BButton>
+              <div
+                class="mt-5 freshman d-flex align-items-center justify-content-center"
+              >
+                我没有账号
+              </div>
             </BForm>
           </div>
         </BTab>
       </BTabs>
-      <div class="mt-5 freshman d-flex align-items-center justify-content-center">我没有账号</div>
-
 
       <template #footer>
-        <div class="wechat-login" @click="createToast(toast,'敬请期待','暂未开通','info')">
+        <div
+          class="wechat-login"
+          @click="createToast(toast, '敬请期待', '暂未开通', 'info')"
+        >
           <Icon color="black" icon="bi:wechat" /> 微信登录
         </div>
       </template>
@@ -114,10 +201,16 @@ const phoneData = reactive({
 </template>
 
 <style lang="scss" scoped>
-.wechat-login{
-  cursor: pointer;
+:deep(.nav-link) {
+  color: black;
+}
+:deep(.nav-pills) {
+  gap: 2rem;
 }
 
+.wechat-login {
+  cursor: pointer;
+}
 
 .top_pic {
   width: 50px;
---
+++ src/Utils/reks-login-regex.ts
@@ -0,0 +1,38 @@
+// 邮箱正则
+const accountValidation = (account: string, password: string) => {
+  try {
+    if (
+      account.length == 0 &&
+      account.length > 2 &&
+      password.length == 0 &&
+      password.length < 6
+    ) {
+      return false;
+    }
+    var emailRegex =
+      /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+    emailRegex.test(account) ? true : false;
+  } catch (e) {
+    console.warn("账号验证失败:", e);
+    return false;
+  }
+};
+
+// 手机号正则
+const phoneValidation = (phone: string,code:string) => {
+  try {
+    // 请求
+
+    code ? code.length === 5 : false;
+
+    var phoneRegex = /^1[3-9]\d{9}$/;
+
+    phoneRegex.test(phone) ? true : false;
+  } catch (e) {
+    console.warn("手机号验证失败:", e);
+    return false;
+  }
+};
+
+export { accountValidation, phoneValidation };
