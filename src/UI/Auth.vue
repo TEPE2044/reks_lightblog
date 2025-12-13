87c9947164ed1e2d6fff68253f8e875e534a5aa7
JackyView <jackyview2004@163.com>
2025-12-13 18:05:58 +0800

    update:对bootstrap-vue-next进行了升级；完善了Auth的账号登录 feat:更改了消息栏的样式

--- src/UI/Auth.vue
+++ src/UI/Auth.vue
@@ -3,7 +3,7 @@ import { useToast, useToggle } from "bootstrap-vue-next";
 import { phoneRegex } from "../Utils/reks-login-regex";
 import type { PhoneData, AccountData } from "../Utils/reks-interface";
 import { createToast } from "../Utils/reks-toast";
-import { reactive, ref, shallowRef } from "vue";
+import { reactive, ref, shallowRef, watchEffect } from "vue";
 import Vcode from "vue3-puzzle-vcode";
 import reapi from "../Requests/reapi";
 import { useCountdown } from "@vueuse/core";
@@ -18,6 +18,8 @@ const toast = useToast();
 
 // 验证码锁
 const codeActive = ref(false);
+
+//TODO:超过一定登录次数，提示：你似乎不是人类，请稍后再试
 
 /*
 puzzle
@@ -129,7 +131,7 @@ const sendCode = async () => {
           );
         }
       } catch (e) {
-        console.error("发送验证码失败:",e)
+        console.error("发送验证码失败:", e)
       }
     });
   } catch (e) {
@@ -154,7 +156,7 @@ const loginbyPhone = async () => {
   return res.data;
 };
 
-import {useDebounceFn} from "@vueuse/core";
+import { useDebounceFn } from "@vueuse/core";
 const submitPhoneData = useDebounceFn(async () => {
   if (phoneData.iaccept === false) {
     createToast(toast, "登录失败", "请同意用户协议和隐私政策", "warning");
@@ -191,83 +193,112 @@ const accountData = reactive<AccountData>({
   password: "",
 });
 
-const loginbyAccount = () => {
-  //TODO: 账号登录功能待实现
-};
+import { hashPsw } from "../Utils/reks-crypto";
+
+const loginbyAccount = async (hashpsw: string) => {
+  const res = await reapi({
+    method: "POST",
+    // url: "/auth/login-by-account",
+    url: "/auth/fake-login-by-account",
+    data: {
+      account: accountData.account,
+      password: hashpsw,
+    },
+  });
+  return res.data;
+}
+
+// 更优雅的写法2026/12/13（🐧跳舞）
+const sumbitAccountData = useDebounceFn(() => {
+  // 账号登录功能待实现
+  const check = [
+    { valid: !!accountData.account && accountData.account.length === 11, msg: "请输入正确的账号" },
+    { valid: phoneRegex(accountData.account), msg: "账号格式不正确" },
+    { valid: accountData.password && accountData.password.length >= 6, msg: "请输入正确的密码" },
+  ]
+  for (const item of check) {
+    if (!item.valid) {
+      createToast(toast, "登录失败", item.msg, "warning");
+      return;
+    }
+  }
+
+  try {
+    // TODO:密码加密
+    const hashed_password = hashPsw(accountData.password);
+    openPuzzle(async () => {
+      try {
+        const res = await loginbyAccount(hashed_password);
+        console.log("账号登录成功:", res);
+        if (res.token) {
+          // 存储token
+          user.userLogin(res.token);
+          createToast(toast, "登录成功", "欢迎回来", "success");
+          emd.hide();
+          reset();
+        }
+      } catch (e) {
+        console.error("账号登录失败:", e);
+        createToast(toast, "登录失败", "网络或服务错误，请稍后重试", "danger");
+      }
+    })
+  } catch (e) {
+    console.error("账号登录失败:", e);
+    createToast(toast, "登录失败", "网络或服务错误，请稍后重试", "danger");
+  }
+
+}, 1000);
+
+watchEffect(() => {
+  console.log("accept", phoneData.iaccept);
+  console.log("account", accountData.account.length);
+});
+
 </script>
 
 <template>
   <div class="auth">
     <BButton @click="emd.show()" variant="primary">登录</BButton>
 
-    <BModal
-      id="easy-login-box"
-      title="登录"
-      ok-title="登录"
-      no-header-close
-      footer
-      centered
-    >
-      <div
-        class="lgo d-flex align-items-center justify-content-center gap-2 my-3"
-      >
-        <img
-          src="/reks.svg"
-          class="img-thumbnail top_pic mb-3"
-          alt="reks_label.png"
-        />
+    <BModal id="easy-login-box" title="登录" ok-title="登录" no-header-close footer centered>
+      <div class="lgo d-flex align-items-center justify-content-center gap-2 my-3">
+        <img src="/reks.svg" class="img-thumbnail top_pic mb-3" alt="reks_label.png" />
         <figure class="figure">
-          <figcaption>Rekindle Everything</figcaption>
+          <figcaption>ReKindle Everything</figcaption>
         </figure>
       </div>
-      <BTabs content-class="mt-3" align="center" lazy pills>
+
+      <div class="form w-75 mx-auto" v-if="isShow">
+        <div class="box box-show" v-if="isShow === true">
+          <Vcode :show="isShow" type="inside" @success="onSuccess" />
+          <BButton class="d-flex justify-content-center align-items-center mt-4" @click="cancelLogin">取消登录</BButton>
+        </div>
+      </div>
+      <BTabs content-class="mt-3" align="center" lazy pills v-else>
         <BTab title="账号登录">
           <div class="form w-75 mx-auto">
-            <BForm class="mx-auto">
-              <BFormFloatingLabel
-                class="mt-4 mb-3"
-                label="账号"
-                label-for="user-account"
-              >
-                <BFormInput
-                  autocomplete="cellphone"
-                  type="email"
-                  id="user-account"
-                  placeholder="请输入账号"
-                  v-model="accountData.account"
-                />
+            <BForm class="mx-auto" validated>
+              <BFormFloatingLabel class="mt-4 mb-3" label="账号" label-for="user-account">
+                <BFormInput autocomplete="cellphone" type="tel" id="user-account" placeholder="请输入账号(手机号)" is-valid
+                  required v-model="accountData.account" />
+                <div class="invalid-feedback">
+                  请输入账号(手机号)
+                </div>
               </BFormFloatingLabel>
 
-              <BFormFloatingLabel
-                class="mb-2"
-                label="密码"
-                label-for="user-password"
-              >
-                <BFormInput
-                  autocomplete="current-password"
-                  type="password"
-                  id="user-password"
-                  placeholder="请输入密码"
-                  v-model="accountData.password"
-                />
+              <BFormFloatingLabel class="mb-2" label="密码" label-for="user-password">
+                <BFormInput autocomplete="current-password" type="password" id="user-password" placeholder="请输入密码"
+                  v-model="accountData.password" is-valid required />
+                <div class="invalid-feedback">
+                  请输入密码
+                </div>
               </BFormFloatingLabel>
 
-              <BButton
-                class="w-100 mt-5"
-                variant="primary"
-                @click="loginbyAccount"
-                >登录</BButton
-              >
+              <BButton class="w-100 mt-2" variant="primary" @click="sumbitAccountData">登录</BButton>
 
-              <div
-                class="freshman mt-3 d-flex align-items-center justify-content-center"
-              >
+              <div class="freshman mt-3 d-flex align-items-center justify-content-center">
                 我没有账号
-                <BPopover
-                  target=".freshman"
-                  placement="bottom"
-                  triggers="hover focus"
-                >
+                <BPopover target=".freshman" placement="bottom" triggers="hover focus">
                   首次使用，请使用短信登录注册账号
                 </BPopover>
               </div>
@@ -276,71 +307,32 @@ const loginbyAccount = () => {
         </BTab>
 
         <BTab title="短信登录">
-          <div class="form w-75 mx-auto" v-if="isShow">
-            <div class="box box-show" v-if="isShow === true">
-              <Vcode :show="isShow" type="inside" @success="onSuccess" />
-              <BButton class="d-flex justify-content-center align-items-center mt-4" @click="cancelLogin">取消登录</BButton>
-            </div>
-          </div>
-          <div class="form w-75 mx-auto" v-else>
+          <div class="form w-75 mx-auto">
             <BForm class="mx-auto" validated>
-              <BFormFloatingLabel
-                class="mt-4 mb-2"
-                label="手机号"
-                label-for="user-phone"
-              >
-                <BFormInput
-                  type="tel"
-                  id="user-phone"
-                  placeholder="请输入手机号"
-                  v-model="phoneData.phone"
-                  is-valid
-                  required
-                />
+              <BFormFloatingLabel class="mt-4 mb-2" label="手机号" label-for="user-phone">
+                <BFormInput type="tel" id="user-phone" placeholder="请输入手机号" v-model="phoneData.phone" is-valid
+                  required />
                 <div class="invalid-feedback">请输入手机号</div>
               </BFormFloatingLabel>
 
               <BInputGroup class="mb-2">
                 <BFormFloatingLabel label="验证码" label-for="user-code">
-                  <BFormInput
-                    type="text"
-                    id="user-code"
-                    placeholder="请输入验证码"
-                    v-model="phoneData.code"
-                    :disabled="phoneData.phone.length === 0"
-                    required
-                  />
+                  <BFormInput type="text" id="user-code" placeholder="请输入验证码" v-model="phoneData.code"
+                    :disabled="phoneData.phone.length === 0" required />
                 </BFormFloatingLabel>
 
-                <BButton
-                  v-if="!codeActive"
-                  text="button"
-                  :disabled="phoneData.phone.length === 0"
-                  variant="primary"
-                  @click="sendCode"
-                  >获取验证码</BButton
-                >
-                <BButton v-else text="button" :disabled="true" variant="primary"
-                  >{{ remaining }}s</BButton
-                >
+                <BButton v-if="!codeActive" text="button" :disabled="phoneData.phone.length === 0" variant="primary"
+                  @click="sendCode">获取验证码
+                </BButton>
+                <BButton v-else text="button" :disabled="true" variant="primary">{{ remaining }}s</BButton>
               </BInputGroup>
-
-              <BFormCheckbox
-                v-model="phoneData.iaccept"
-                name="checkbox-1"
-                class="mt-4 mb-2"
-              >
-                我已阅读并同意<a href="#">用户协议</a>和<a href="#">隐私政策</a>
-              </BFormCheckbox>
-
-              <BButton
-                :disabled="phoneData.code.length === 0"
-                class="w-100 mt-4 mb-3"
-                variant="primary"
-                @click="submitPhoneData"
-                >登录</BButton
-              >
             </BForm>
+            <BFormCheckbox v-model="phoneData.iaccept" name="checkbox-1" class="mt-4 mb-2" :state="phoneData.iaccept">
+              我已阅读并同意<BLink class="prt" href="#">用户协议</BLink>和<BLink class="prt" href="#">隐私政策</BLink>
+            </BFormCheckbox>
+
+            <BButton :disabled="phoneData.code.length === 0" class="w-100 mt-4 mb-3" variant="primary"
+              @click="submitPhoneData">登录</BButton>
           </div>
         </BTab>
       </BTabs>
@@ -356,6 +348,14 @@ const loginbyAccount = () => {
 </template>
 
 <style lang="scss" scoped>
+// :deep(.prt){
+//   color: black !important;
+
+// }
+.prt {
+  color: rgb(209, 173, 251);
+}
+
 :deep(.nav-link) {
   color: black;
 }
@@ -363,11 +363,13 @@ const loginbyAccount = () => {
 :deep(.nav-pills) {
   gap: 2rem;
 }
+
 :deep(.vue-puzzle-vcode) {
   display: flex;
   justify-content: center;
   align-items: center;
 }
+
 .wechat-login {
   cursor: pointer;
 }
--- src/UI/MessageBar.vue
+++ src/UI/MessageBar.vue
@@ -1,36 +1,49 @@
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
-import { ref } from "vue";
-
-const sys = ref(false);
+import { shallowRef } from "vue";
+import { userStore } from "../Store/user";
+const user = userStore()
+const sys = shallowRef(false);
 </script>
 <template>
-    <div
-      class="message-bar rounded d-flex justify-content-around align-items-center p-1 gap-2"
+  <div v-if="user.isLoggedIn"
+    class="message-bar d-flex justify-content-around align-items-center p-2 gap-2"
+  >
+    <BButton @click="sys = !sys" class="position-relative" variant="light">
+      <Icon icon="bi:bell"/>
+      <BBadge
+        v-show="sys"
+        dot-indicator
+        variant="danger"
+        class="position-absolute top-0 start-100 translate-middle"
+      />
+    </BButton>
+    <BButton @click="sys = !sys" class="position-relative" variant="light">
+      <Icon icon="bi:envelope"/>
+      <BBadge
+        v-show="sys"
+        dot-indicator
+        variant="danger"
+        class="position-absolute top-0 start-100 translate-middle"
+      />
+    </BButton>
+    <BDropdown
+      variant="light"
+      toggle-class="text-decoration-none"
+      class="position-relative"
+      no-caret
     >
-      <BButton class="position-relative bb" variant="light">
-        <Icon icon="bi:megaphone"/>
-        <BBadge
-          v-show="sys"
-          dot-indicator
-          variant="danger"
-          class="position-absolute top-0 start-100 translate-middle"
-        />
-      </BButton>
-      <BButton class="position-relative bb" variant="light">
-        <Icon icon="bi:envelope"/>
-      </BButton>
-      <BButton class="position-relative bb" variant="light">
-        <Icon icon="bi:person-circle"/>
-      </BButton>
-    </div>
-
+      <template #button-content>
+        <Icon icon="bi:person"/>
+      </template>
+      <BDropdownItem><Icon icon="bi:person"/>  我的空间</BDropdownItem>
+      <BDropdownItem><Icon icon="bi:shop"/>  积分商城</BDropdownItem>
+      <BDropdownItem><Icon icon="bi:upload"/>  投稿管理</BDropdownItem>
+      <BDropdownDivider />
+      <BDropdownItem><Icon icon="bi:patch-question"/>  帮助</BDropdownItem>
+    </BDropdown>
+  </div>
 </template>
 
 <style scoped>
-.message-bar {
-  .bb{
-    background-color: rgba(255, 255, 255, 0.671);
-  }
-}
 </style>
