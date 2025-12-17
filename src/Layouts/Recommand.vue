2ba35e5e716d3621fd26316c8af9aa2a25c2dcc2
JackyView <jackyview2004@163.com>
2025-12-18 00:06:05 +0800

    feat:对样式进行了修改

--- src/Layouts/Recommand.vue
+++ src/Layouts/Recommand.vue
@@ -3,21 +3,13 @@
 </script>
 
 <template>
-  <BContainer class="recommand mb-4">
-    <figure class="mt-3">
-      <blockquote class="blockquote">
-        <p class="label-word">ReKindle Everything</p>
-      </blockquote>
-    </figure>
-
-    <BCard class="recommand-tab mt-2 mb-4" no-body>
+  <div class="recommand mb-4">
+    <BCard class="recommand-tab mb-4" no-body>
       <BCardHeader header-tag="nav">
-        <BNav card-header tabs>
-          <BNavItem to="/news/" exact exact-active-class="active"
-            >个性化推荐
+        <BNav align="center" pills>
+          <BNavItem to="/news/" exact exact-active-class="active">推荐
           </BNavItem>
-          <BNavItem to="/subscribe/" exact exact-active-class="active"
-            >我的订阅
+          <BNavItem to="/subscribe/" exact exact-active-class="active">订阅
           </BNavItem>
         </BNav>
       </BCardHeader>
@@ -26,42 +18,44 @@
         <RouterView />
       </BCardBody>
     </BCard>
-  </BContainer>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../Asset/CustomStyle/global.scss";
 
-:deep(.nav-link){
+:deep(.nav-link) {
   color: black !important;
 }
-:deep(.nav-link.active){
-  color: firebrick !important;
-}
-:deep(.card-header){
-  background-color: rgba(255, 236, 201, 0.219) !important;
+
+:deep(.nav-pills .nav-link.active) {
+  background-color: rgba(178, 34, 34, 0.126) !important;
 }
 
 
 .recommand {
+  border-radius: 12px;
+
   .label-word {
-   color: #660909;
+    color: #660909;
   }
 
   min-height: 400px;
-  @extend %reks-card-box;
 
   .recommand-tab {
+
     /* 保证内容区高度一致，避免切换时父容器高度跳动 */
     .content {
       min-height: 300px;
-      position: relative; /* 为绝对定位的进入元素提供上下文 */
+      position: relative;
+      /* 为绝对定位的进入元素提供上下文 */
     }
 
     /* 如果你使用 <transition> 包裹 RouterView，可以让进入的组件绝对定位 */
     .recommand-content {
       position: absolute;
-      inset: 0; /* top:0; right:0; bottom:0; left:0 */
+      inset: 0;
+      /* top:0; right:0; bottom:0; left:0 */
     }
   }
 }
