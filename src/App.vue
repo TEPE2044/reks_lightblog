a52a2029df378ddb4d5229ed157d5ed9fdb389b1
JackyView <jackyview2004@163.com>
2026-01-11 17:43:35 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
+<script setup lang="ts">
+import { BApp } from "bootstrap-vue-next";
+import Header from "./Components/Header.vue";
+import MainLayout from "./Layouts/MainLayout.vue";
+import Footer from "./Components/Footer.vue";
+import Auth from "./Widgets/Auth.vue";
+import MusicPlayer from "./Widgets/MusicPlayer.vue";
+
+</script>
 <!-- App.vue -->
 <template>
   <BApp>
     <header>
-      <Header />
+      <Header/>
     </header>
     <main>
       <MainLayout />
@@ -14,18 +23,11 @@
     <Teleport to="body">
       <Auth :show-trigger="false" />
     </Teleport>
-    
+
   </BApp>
 </template>
 
-<script setup lang="ts">
-import { BApp } from "bootstrap-vue-next";
-import Header from "./Components/Header.vue";
-import MainLayout from "./Layouts/MainLayout.vue";
-import Footer from "./Components/Footer.vue";
-import Auth from "./Widgets/Auth.vue";
-import MusicPlayer from "./Widgets/MusicPlayer.vue";
-</script>
+
 
 <style lang="scss" scoped>
 header {
@@ -39,10 +41,8 @@ header {
 main {
   flex: 1 0 auto;
   background-color: rgb(254, 249, 239);
-  background-image: linear-gradient(
-    62deg,
-    rgba(245, 222, 179, 0.43) 0%,
-    rgba(245, 222, 179, 0.31) 100%
-  );
+  background-image: linear-gradient(62deg,
+      rgba(245, 222, 179, 0.43) 0%,
+      rgba(245, 222, 179, 0.31) 100%);
 }
 </style>
--- src/Components/Navbar.vue
+++ src/Components/Navbar.vue
@@ -3,14 +3,14 @@
 </script>
 
 <template>
-    <div class="navbar">
+    <div class="navbar" >
         <BContainer>
             <BNavbarNav class="main-nav flex-row gap-4 align-items-center">
                 <BNavItem class="section" id="home-item" to="/home">
                     <BLink to="/home">首页</BLink>
                 </BNavItem>
                 <BNavItem class="section" id="community-item" to="/hub">
-                      <BLink to="/hub">社区</BLink>
+                    <BLink to="/hub">社区</BLink>
                 </BNavItem>
             </BNavbarNav>
         </BContainer>
@@ -19,10 +19,13 @@
 
 <style lang="scss" scoped>
 .navbar {
+    padding: 0;
     background: rgba(254, 240, 215, 0.85);
-    a{
+
+    a {
         font-weight: bold;
     }
+
     .main-nav {
         .section {
             // 保持与激活态相同的宽度，使用 transparent 避免布局抖动
