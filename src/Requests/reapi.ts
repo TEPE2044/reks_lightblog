dfa0dcec396aa3d416dfa427cb4191e54522928f
JackyView <jackyview2004@163.com>
2026-01-24 01:28:59 +0800

    feat:服务器相关修改

--- src/Requests/reapi.ts
+++ src/Requests/reapi.ts
@@ -3,7 +3,7 @@ import { userStore } from "../Store/user";
 
 
 const reapi = axios.create({
-  baseURL: "http://api.rekindlers.top/api/v1/",
+  baseURL: "http://v1.rekindlers.top/api/v1/",
   timeout: 15000,
   withCredentials: true,
   headers: {
