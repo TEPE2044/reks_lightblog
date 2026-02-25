f6bf9e1677ca27230240145b35e75e30fa04cd64
JackyView <jackyview2004@163.com>
2026-02-25 16:27:33 +0800

    feat:加入了graphql订阅！

--- src/Layouts/MainLayout.vue
+++ src/Layouts/MainLayout.vue
@@ -1,5 +1,46 @@
 <script setup lang="ts">
+import { ref, onUnmounted } from 'vue';
+import { wsClient } from '../Requests/ws-regql';
+import { useToast } from 'bootstrap-vue-next';
+import { createToast } from '../Utils/reks-toast';
 
+interface Blog {
+  blogId: number;
+  title: string;
+  authorId: number;
+} 
+const toast = useToast()
+
+function useBlogSubscription() {
+  const latestBlog = ref<Blog | null>(null);
+
+  // 开始订阅，拿到“关闭函数”
+  const unsubscribe = wsClient.subscribe<{ testFollowing: Blog }>(
+    {
+      query: `subscription { testFollowing { blogId title authorId } }`,
+    },
+    {
+      next: ({ data }) => {
+        const blog = data?.testFollowing ?? null;
+        latestBlog.value = blog;
+        if (blog) {
+          createToast(toast, "新消息", blog.title, "primary");
+        }
+      },
+      error: console.error,
+      complete: () => {},
+    },
+  );
+
+  // 组件销毁时停止监听
+  onUnmounted(() => unsubscribe());
+
+  return {
+    latestBlog,
+  };
+}
+
+const { latestBlog } = useBlogSubscription();
 </script>
 
 <template>
---
+++ src/Requests/ws-regql.ts
@@ -0,0 +1,30 @@
+import { createClient } from "graphql-ws";
+import { userStore } from "../Store/user";
+import router from "../Router";
+
+
+// 启动 graph-ws
+export const wsClient = createClient({
+  url: "ws://localhost:12404/api/v1/gql/subql",
+  shouldRetry: () => true,
+  connectionParams: () => getAuthHeaders(),
+});
+
+// 获取
+const getAuthHeaders = () => {
+  const rcode = localStorage.getItem("rcode")
+  const payload = localStorage.getItem("payload");
+  
+  if(!rcode && !payload)
+  {
+    const user = userStore();
+    user.userLogout();
+    alert("身份验证失败，请重新登录");
+    router.replace('/')
+    return
+  }
+  return {
+    Authorization: rcode ? `Bearer ${rcode}` : "",
+    "X-Payload": payload || '',
+  };
+};
