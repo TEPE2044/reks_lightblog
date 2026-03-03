6352f4eef55cfac743bf20432352e1e12217081f
JackyView <jackyview2004@163.com>
2026-03-03 09:47:07 +0800

    feat:推荐区加入内容滚动，取消全局滚动

--- src/Pages/News.vue
+++ src/Pages/News.vue
@@ -1,7 +1,37 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
+import { query_my_blog } from "../Hooks/Blog";
+import type { BlogData } from "../Utils/reks-interface";
+import { useToggle } from "@vueuse/core";
 
-const empty = ref(true);
+
+const blogs = ref<BlogData[]>([]);
+const loading = ref(true)
+const [empty, setEmpty] = useToggle()
+onMounted(async () => {
+  // 先读缓存
+  const cached = localStorage.getItem('blogs')
+  if (cached) {
+    blogs.value = JSON.parse(cached)
+    loading.value = false  // 立即显示，无需等待
+    console.log(loading.value)
+  }
+
+  // 再请求新数据
+  const res = await query_my_blog()
+  blogs.value = res?.blogs
+  if (res == null) {
+    setEmpty(true)
+  }
+  if (blogs.value?.length === 0) {
+    setEmpty(true)
+  } else {
+    setEmpty(false)
+  }
+  localStorage.setItem('blogs', JSON.stringify(res?.blogs))
+  loading.value = false
+})
+
 </script>
 
 <template>
@@ -9,7 +39,7 @@ const empty = ref(true);
     <Empty/>
   </div>
   <div class="ns" v-if="!empty">
-    <!-- <BlogCard v-for="blog in blogs" :key="blog.id" :blog="blog" /> -->
+    <BlogCard v-for="blog in blogs" :key="blog.id" :blog="blog" />
   </div>
 
 </template>
