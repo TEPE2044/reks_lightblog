0e3f4346910fecfa80a8457957b10ee1ccc7fbd9
JackyView <jackyview2004@163.com>
2026-04-02 15:43:53 +0800

    update:针对Music表新字段进行了调整；MusicForm添加了类型选择的按钮，调整了部分逻辑

--- src/Utils/reks-interface.ts
+++ src/Utils/reks-interface.ts
@@ -79,6 +79,7 @@ export interface MusicTwice {
   isOriginal: boolean;
   name: string;
   desc: string;
+  type: number;
   coverURL: string | undefined;
   audioURL: string | undefined;
 }
@@ -87,6 +88,7 @@ export interface MusicData {
   isOriginal: boolean;
   name: string;
   desc: string;
+  type: number;
 }
 
 export interface MusicResponse {
@@ -97,6 +99,8 @@ export interface MusicResponse {
   id: number;
   name: string;
   original: boolean;
+  type: number;
+  related: number;
   rid: number;
   state: number;
   username: string;
