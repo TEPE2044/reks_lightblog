21e1db2b4ca6ea8a72aa4238857bdcae624c1b98
JackyView <jackyview2004@163.com>
2026-04-24 08:49:46 +0800

    update:去掉无用的旧包；启用markdown模块;去掉已迁移的Editor.vue文件

--- src/Utils/reks-test.ts
+++
@@ -1,7 +0,0 @@
-import { ref } from "vue"
-
-
-let something_new = $ref('')
-export const iwantsomedata = () => {
-  console.log("fake data now")
-}
--- src/Utils/wangeditor-init.ts
+++ src/Utils/wangeditor-init.ts
@@ -1,7 +1,11 @@
 import { Boot } from "@wangeditor-next/editor";
+import markdownModule from "@wangeditor-next/plugin-markdown";
 import { MUSIC_CARD_MENU_KEY, MusicCardMenu } from "./reks-editor-modal";
 
 // 全局只需要执行一次（建议在 main.ts import 这个文件）
+// Markdown 快捷语法（# 标题、``` 代码块、列表等），须在创建编辑器之前注册且只注册一次
+Boot.registerModule(markdownModule);
+
 Boot.registerMenu({
   // 音乐卡片菜单注册
   key: MUSIC_CARD_MENU_KEY,
