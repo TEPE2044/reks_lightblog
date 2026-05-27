7675546858349f2ad9f9594a39f8a7ceb4388f35
JackyView <jackyview2004@163.com>
2026-05-27 11:29:02 +0800

    update:文字优化；提高请求超时上限

--- src/Requests/reapi.ts
+++ src/Requests/reapi.ts
@@ -2,17 +2,16 @@ import axios from "axios";
 import { userStore } from "../Store/user";
 import router from "../Router";
 
-
 const reapi = axios.create({
   baseURL: "http://localhost:12404/api/v1/",
-  timeout: 15000,
+  timeout: 30000,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 // 请求拦截器
-reapi.interceptors.request.use(config => {
+reapi.interceptors.request.use((config) => {
   const rcode = localStorage.getItem("rcode");
   const payload = localStorage.getItem("payload");
   if (rcode && payload) {
@@ -20,22 +19,21 @@ reapi.interceptors.request.use(config => {
     config.headers["X-Payload"] = payload;
   }
   return config;
-})
-
+});
 
 // 响应拦截器
 reapi.interceptors.response.use(
-  res => res,
-  async err => {
-    if(err.response?.status === 401) {
+  (res) => res,
+  async (err) => {
+    if (err.response?.status === 401) {
       const user = userStore();
       user.userLogout();
-      router.replace('/')
+      router.replace("/");
     }
-    if(err.response?.status === 429){
-      alert("请求次数已达上限！请勿重复请求")
+    if (err.response?.status === 429) {
+      alert("请求次数已达上限！请勿重复请求");
     }
     return Promise.reject(err);
-  }
-)
+  },
+);
 export default reapi;
